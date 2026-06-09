e3f19e3b28a93e292bf5369e6bd5660d8f26e577
Shravan Choudhary <smchoudhary19093@gmail.com>
2026-06-09 14:57:32 +0530

    feat: changed all hardcoded link to vite-url

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -31,7 +31,7 @@ export default function Home() {
 
     async function fetchLinks() {
         try {
-            const res = await axios.get("http://localhost:5001/api/urls", {
+            const res = await axios.get(`${import.meta.env.VITE_API_URL}/api/urls`, {
                 headers: { token: localStorage.getItem("token") },
             })
             setLinks(res.data.getallurls || [])
@@ -56,7 +56,7 @@ export default function Home() {
 
         try {
             const response = await axios.post(
-                "http://localhost:5001/api/shorten",
+                `${import.meta.env.VITE_API_URL}/api/shorten`,
                 { originalurl: originalUrl, aliasurl: aliasValue },
                 { headers: { token: localStorage.getItem("token") } }
             )
